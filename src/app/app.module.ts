2d15e79a1c78a627d211c8f5da2ffbcb16dfe58b
Ryan <ryan.rodemoyer@icloud.com>
2020-05-02 10:45:19 -0600

    split out separate components guid-generator and guid-info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,15 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { GuidGeneratorComponent } from './components/guid-generator/guid-generator.component';
+
+import { GuidInfoComponent } from './components/guid-info/guid-info.component';
 import { UuidService } from './services/uuid.service';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot([]), ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, GuidGeneratorComponent, GuidInfoComponent ],
   bootstrap:    [ AppComponent ],
   providers: [UuidService]
 })
---
+++ src/app/components/guid-info/guid-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "guid-info",
+  templateUrl: "./guid-info.component.html",
+  styleUrls: ["./guid-info.component.css"]
+})
+export class GuidInfoComponent {
+  constructor(
+    ) {}
+}
